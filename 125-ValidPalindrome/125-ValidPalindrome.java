a5c928a02c52e2ca1259761b1fd0a9e17e38487c
MaheswaraPradeep <101545800+MaheswaraPradeep@users.noreply.github.com>
2024-08-26 20:52:33 +0530

    Update file 8/26/2024, 8:52:33 PM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-
-        while (i < j) {
-            // Skip non-alphanumeric characters from the left
-            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
-                i++;
-            }
-            // Skip non-alphanumeric characters from the right
-            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
-                j--;
-            }
-            // Compare characters ignoring case
-            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
-                return false;
-            }
-            i++;
-            j--;
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        if(s.length()<1){
+            return true;
         }
-        return true;
+       return pal(0,s);
+    }
+    static boolean pal(int i,String s){
+        if(i>=s.length()/2) return true;
+        if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+            return false;
+        }
+        return pal(i+1,s);
     }
 }
