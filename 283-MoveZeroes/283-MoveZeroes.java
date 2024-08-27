cdc637340018783b0c787c5b7806ea49f99ac406
MaheswaraPradeep <101545800+MaheswaraPradeep@users.noreply.github.com>
2024-08-28 00:33:29 +0530

    Update file 8/28/2024, 12:33:29 AM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,14 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
             }
-        }        
+        }
     }
 }
